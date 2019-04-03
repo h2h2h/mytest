6630e2d34ece7d5fe267c1cfc916f98297a0f9ea
huanghui <415746882@qq.com>
2019-04-03 10:23:35 +0800

    branch 提交

--- my-app/src/main/java/com/huang/app/App.java
+++ my-app/src/main/java/com/huang/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!--branch---2019-0403" );
+        System.out.println( "Hello World!--branch---2019-0403111" );
     }
 }
