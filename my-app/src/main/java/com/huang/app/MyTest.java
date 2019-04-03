812ea50695f38456543638749adf37c8d7ba55b6
huanghui <415746882@qq.com>
2019-04-03 10:27:52 +0800

    branch 提交测试问卷

---
+++ my-app/src/main/java/com/huang/app/MyTest.java
@@ -0,0 +1,13 @@
+package com.huang.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class MyTest
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!--branch---2019-0403111" );
+    }
+}
